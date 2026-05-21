c6688122b06c41b95dfe5c819028842b96db440a
Fayez <fayezahmad827@gamil.com>
2026-05-21 13:20:07 +0530

    minor change

--- version.js
+++
@@ -1,6 +0,0 @@
-{
-  "versionCode": 1,
-  "versionName": "1.01.0",
-  "apkUrl": "https://github.com/FAYEZ087/BunkCalc_App/releases/download/v1.1.0/BunkCalc.apk",
-  "changelog": "Initial release"
-}
